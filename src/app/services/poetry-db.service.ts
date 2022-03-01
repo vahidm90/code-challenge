afb27a785875447c91ddd911ad1a972ed99d9756
VahidM <v.mohagheghi@asax.local>
2022-03-01 16:39:22 +0330

    feature: poetry-db.service.ts: method to get poems from API
    add 'getRandomPoems' method to get 20 random poems from API;
    add 'catchError' to display API call errors in console;

--- src/app/services/poetry-db.service.ts
+++ src/app/services/poetry-db.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { catchError, Observable, of } from 'rxjs';
+import { IPoemModel } from '../interfaces/poem.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PoetryDbService {
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
+
+  getRandomPoems(): Observable<IPoemModel[]> {
+    return this._http.get<IPoemModel[]>('https://poetrydb.org/random/20')
+      .pipe(catchError(err => {
+        console.error(err);
+        return of([]);
+      //  todo: an API call error Handler service which displays toast upon errors
+      }));
+  }
+
 }
