6b2e10b19325beecc12bc1d06a8b03f9993850f7
VahidM <v.mohagheghi@asax.local>
2022-03-01 16:47:40 +0330

    feature: app.component.ts: component setup
    add 'OnDestroy' hook to limit subscription life;
    add click handler for fetch button;
    add member to store fetched data;
    inject service to get data;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { PoetryDbService } from './services/poetry-db.service';
+import { Subject, takeUntil } from 'rxjs';
+import { IPoemModel } from './interfaces/poem.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'bejoynd-challenge';
+export class AppComponent implements OnDestroy {
+
+  poems!: IPoemModel[];
+
+  private _destroy$ = new Subject<void>();
+
+  constructor(private _poetryService: PoetryDbService) {}
+
+  ngOnDestroy() {
+    this._destroy$.next();
+    this._destroy$.complete();
+  }
+
+  onFetchButtonClick(): void {
+    this._poetryService.getRandomPoems()
+      .pipe(takeUntil(this._destroy$))
+      .subscribe(poems => this.poems = poems);
+  }
+
 }
